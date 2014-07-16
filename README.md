c10dc119cd48c9028039516edde2d1ba6947ea49
RGkevin <rgkevin.lopez@gmail.com>
2014-07-16 12:28:24 -0600

    adding docs

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ http://rgkevin.github.io/datetimeRangePicker/
 
 ## Dependencies
 - required:
-	[TODO]
+    [http://getbootstrap.com bootstrap] bootstrap
+	[http://angular-ui.github.io/bootstrap/ ui.bootstrap]
+	[https://github.com/Venturocket/angular-slider angular-slider]
 - optional
 	[TODO]
 
@@ -18,34 +20,13 @@ See `bower.json` and `index.html` in the `gh-pages` branch for a full list / mor
 	1. Bower
 		1. add `"datetimeRangePicker": "latest"` to your `bower.json` file then run `bower install` OR run `bower install datetimeRangePicker`
 2. include the files in your app
-	1. `eRangePicker.min.js`
-	2. `eRangePicker.less` OR `eRangePicker.min.css` OR `eRangePicker.css`
+	1. `range-picker.min.js`
+	2. `range-picker.less` OR `range-picker.min.css` OR `range-picker.css`
 3. include the module in angular (i.e. in `app.js`) - `rgkevin.datetimeRangePicker`
 
 See the `gh-pages` branch, files `bower.json` and `index.html` for a full example.
 
 
 ## Documentation
-See the `eRangePicker.js` file top comments for usage examples and documentation
-https://github.com/rgkevin/datetimeRangePicker/blob/master/eRangePicker.js
-
-
-## Development
-
-1. `git checkout gh-pages`
-	1. run `npm install && bower install`
-	2. write your code then run `grunt`
-	3. git commit your changes
-2. copy over core files (.js and .css/.less for directives) to master branch
-	1. `git checkout master`
-	2. `git checkout gh-pages eRangePicker.js eRangePicker.min.js eRangePicker.less eRangePicker.css eRangePicker.min.css`
-3. update README, CHANGELOG, bower.json, and do any other final polishing to prepare for publishing
-	1. git commit changes
-	2. git tag with the version number, i.e. `git tag v1.0.0`
-4. create github repo and push
-	1. [if remote does not already exist or is incorrect] `git remote add origin [github url]`
-	2. `git push origin master --tags` (want to push master branch first so it is the default on github)
-	3. `git checkout gh-pages`
-	4. `git push origin gh-pages`
-5. (optional) register bower component
-	1. `bower register datetimeRangePicker [git repo url]`
+See the `range-picker.js` file top comments for usage examples and documentation
+https://github.com/rgkevin/datetimeRangePicker/blob/master/range-picker.js
---
+++ range-picker.js
@@ -0,0 +1,124 @@
+/**
+@toc
+
+@param {Object} scope (attrs that must be defined on the scope (i.e. in the controller) - they can't just be defined in the partial html). REMEMBER: use snake-case when setting these on the partial!
+TODO
+
+@param {Object} attrs REMEMBER: use snake-case when setting these on the partial! i.e. my-attr='1' NOT myAttr='1'
+TODO
+
+@dependencies
+TODO
+
+@usage
+partial / html:
+TODO
+
+controller / js:
+TODO
+
+//end: usage
+*/
+
+'use strict';
+
+angular.module('rgkevin.datetimeRangePicker', [])
+    .filter('rgTime', [function () {
+        /**
+         * input should be a number of minutes to be parsed
+         * @param {input} number of minutes
+         * @param {type} true = 00:00:00 | false = 00:00 am or pm
+         */
+        return function (input, type) {
+            var
+                hours = parseInt( input / 60, 10 ),
+                minutes = parseInt( input - (hours * 60) ) || '00',
+                meridian = type ? ':00' : ( hours >= 12 && hours !== 24 ? ' pm' : ' am' );
+
+            return (!type && hours > 12 ? (hours === 24 ? '00' : (hours - 12 < 10 ? '0': '' ) + (hours - 12) ) : (hours < 10 ? '0' : '') + hours) + ':' + minutes + meridian;
+        };
+    }])
+    .directive('rgRangePicker', [ function () {
+
+	return {
+		restrict: 'A',
+		scope: {
+            data: '=rgRangePicker',
+            labels: '='
+		},
+
+		// replace: true,
+		template: function(element, attrs) {
+			var defaultsAttrs ={
+                data: {
+                    date: {
+                        from: new Date(),
+                        to: new Date()
+                    },
+                    time: {
+                        from: 0,
+                        to: 1440 // minutes in a day
+                    }
+                }
+			};
+			for(var xx in defaultsAttrs) {
+				if(attrs[xx] ===undefined) {
+					attrs[xx] =defaultsAttrs[xx];
+				}
+			}
+
+			return '<div class="rg-range-picker">' +
+                        '<div class="rg-range-picker-box well">' +
+                            '<div class="rg-range-picker-calendars">' +
+                                '<div class="rg-range-picker-calendar-box">' +
+                                    '<h5 class="rg-range-picker-calendar-label" ng-bind-template="{{labels.date.from}}"></h5>' +
+                                    '<datepicker ng-model="data.date.from" max-date="data.date.to" show-weeks="false" class="clean-calendar"></datepicker>' +
+                                '</div>' +
+                                '<div class="rg-range-picker-calendar-box right">' +
+                                    '<h5 class="rg-range-picker-calendar-label" ng-bind-template="{{labels.date.to}}"></h5>' +
+                                    '<datepicker ng-model="data.date.to" min-date="data.date.from" show-weeks="false" class="clean-calendar"></datepicker>' +
+                                '</div>' +
+                            '</div>' +
+                            '<div class="rg-range-picker-slider">' +
+                                '<div class="rg-range-picker-slider-labels">' +
+                                    '<div class="row">' +
+                                        '<div class="rg-range-picker-divider xs-hidden"><span class="label">to</span></div>' +
+                                        '<div class="col-sm-6 text-center"><span class="label label-range-picker">{{data.time.from | rgTime:data.time.hours24}}</span></div>' +
+                                        '<div class="col-sm-6 text-center"><span class="label label-range-picker">{{data.time.to | rgTime:data.time.hours24}}</span></div>' +
+                                    '</div>' +
+                                '</div>' +
+                                '<div slider class="clean-slider" ng-model="data.time.from" ng-model-range="data.time.to" floor="{{data.time.dFrom}}" ceiling="{{data.time.dTo}}" buffer="{{data.time.minRange || 1}}" step="{{data.time.step || 1}}" step-width="{{data.time.step || 1}}" precision="0" stretch="3"></div>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>';
+		},
+
+		link: function(scope, element, attrs) {
+            console.log('scope, element, attrs', scope, element, attrs);
+            // define labels
+            var
+                defaultLabels = {
+                    date: {
+                        from: 'Start date',
+                        to: 'End date'
+                    }
+                },
+                timeDefaults = {
+                    from: 480, // default low value
+                    to: 1020, // default high value
+                    dFrom: 0, // lowest integer
+                    dTo: 1440, // highest integer
+                    step: 15, // step width
+                    minRange: 15, // min range
+                    hours24: true // true for 00:00:00 format and false for 00:00 am or pm
+                };
+
+            scope.labels = angular.extend(defaultLabels, scope.labels);
+            scope.data.time = angular.extend(timeDefaults, scope.data.time);
+
+		},
+
+		controller: function($scope, $element, $attrs) {
+		}
+	};
+}]);
