cb2d09a723cac6243a6dfdcbff20bb91697a258c
RGkevin <rgkevin.lopez@gmail.com>
2014-08-11 12:10:14 -0600

    adding new options

--- pages/home/HomeCtrl.js
+++ pages/home/HomeCtrl.js
@@ -4,11 +4,13 @@
 'use strict';
 
 angular.module('myApp').controller('HomeCtrl', ['$scope', function($scope) {
-	//TODO - put any directive code here
+
 	$scope.myDatetimeRange = {
 		date: {
 			from: new Date(),
-			to: new Date()
+			to: new Date(),
+            min: new Date(),
+            max: new Date()
 		},
 		time: {
 			from: 480, // default low value
@@ -34,5 +36,25 @@ angular.module('myApp').controller('HomeCtrl', ['$scope', function($scope) {
     $scope.whenTimeChange = function (data) {
         $scope.whentimechangedata = data;
     };
+
+    // show only time slider
+    $scope.timeRangePicker = {
+        time: {
+            from: 480, // default low value
+            to: 1020, // default high value
+            step: 15, // step width
+            minRange: 15, // min range
+            hours24: false // true for 24hrs based time | false for PM and AM
+        }
+    };
+
+    // show only date pickers
+    $scope.dateRangePicker = {
+        date: {
+            from: new Date(),
+            to: new Date()
+        }
+    };
+
 
 }]);
